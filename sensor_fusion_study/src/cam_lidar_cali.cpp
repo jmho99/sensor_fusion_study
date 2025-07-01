6ea0319ed0df525e54b9f14882686ddf88b95ef3
jmho99 <bwve6g@gmail.com>
2025-07-01 17:16:03 +0900

    25/07/01

--- sensor_fusion_study/src/cam_lidar_cali.cpp
+++ sensor_fusion_study/src/cam_lidar_cali.cpp
@@ -25,9 +25,9 @@ public:
 
         pub_plane_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("plane_points", 10);
         pub_checker_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("checker_points", 10);
-        pub_lidar2cb_points_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("lidar2cb_points", 10);
+        pub_lidar2cam_points_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("lidar2cam_points", 10);
         pub_cb_points_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("cb_object_points", 10);
-        pub_tf_points_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("tf_points", 10);
+        pub_cam_points_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("cam_points", 10);
 
         // 타이머로 주기적 퍼블리시
         timer_ = this->create_wall_timer(
@@ -46,30 +46,31 @@ private:
     std::string pcd_path_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_plane_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_checker_;
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_lidar2cb_points_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_lidar2cam_points_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_cb_points_;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_tf_points_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_cam_points_;
     sensor_msgs::msg::PointCloud2 plane_msg_;
     sensor_msgs::msg::PointCloud2 checker_msg_;
-    sensor_msgs::msg::PointCloud2 cloud_msg_;
+    sensor_msgs::msg::PointCloud2 lidar2cam_points_;
     sensor_msgs::msg::PointCloud2 cb_points_msg_;
-    sensor_msgs::msg::PointCloud2 tf_points_msg_;
+    sensor_msgs::msg::PointCloud2 cam_points_msg_;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::TimerBase::SharedPtr timer__;
     cv::Size board_size_;                       // Chessboard parameters
     double square_size_;                        // Chessboard parameters
     cv::Mat camera_matrix_, distortion_coeffs_; // Camera intrinsics
     cv::Mat cb2cam_rvec_, cb2cam_tvec_;
-    cv::Mat lidar2cb_R_, lidar2cb_t_;
+    cv::Mat lidar2cam_R_, lidar2cam_t_;
 
     void timerCallback()
     {
         plane_msg_.header.stamp = this->now();
         pub_plane_->publish(plane_msg_);
         pub_checker_->publish(checker_msg_);
-        pub_lidar2cb_points_->publish(cloud_msg_);
+        pub_lidar2cam_points_->publish(lidar2cam_points_);
         pub_cb_points_->publish(cb_points_msg_);
-        pub_tf_points_->publish(tf_points_msg_);
+        pub_cam_points_->publish(cam_points_msg_);
     }
 
     void initializedParameters()
@@ -84,9 +85,10 @@ private:
                               8.2971299771431115e-03, -4.5215579888173568e-03,
                               -7.7949268098546165e-01);
 
-        auto relative_path = ament_index_cpp::get_package_share_directory("sensor_fusion_study");
-        img_path_ = relative_path + "/capture/image.png";
-        pcd_path_ = relative_path + "/capture/pointcloud.pcd";
+        std::string absolute_path = "/home/antlab/fusion_study_ws/src/sensor_fusion_study";
+        // auto absolute_path = ament_index_cpp::get_package_share_directory("/src/sensor_fusion_study");
+        img_path_ = absolute_path + "/capture/image.png";
+        pcd_path_ = absolute_path + "/capture/pointcloud.pcd";
     }
 
     void timer_callback()
@@ -198,6 +200,20 @@ private:
             RCLCPP_ERROR(this->get_logger(), "solvePnP failed!");
             return;
         }
+
+        cv::Mat R;
+        cv::Rodrigues(cb2cam_rvec_, R);
+        std::vector<cv::Point3f> cb2cam_points;
+        cv::transform(object_points, cb2cam_points, R);
+        for (auto &pt : cb2cam_points)
+        {
+            pt.x += cb2cam_tvec_.at<double>(0);
+            pt.y += cb2cam_tvec_.at<double>(1);
+            pt.z += cb2cam_tvec_.at<double>(2);
+        }
+
+        point3fVectorToPointCloud2(cb2cam_points, cam_points_msg_, "map", this->now());
+
         RCLCPP_INFO(this->get_logger(), "PnP rvec: [%f %f %f]", cb2cam_rvec_.at<double>(0), cb2cam_rvec_.at<double>(1), cb2cam_rvec_.at<double>(2));
         RCLCPP_INFO(this->get_logger(), "PnP tvec: [%f %f %f]", cb2cam_tvec_.at<double>(0), cb2cam_tvec_.at<double>(1), cb2cam_tvec_.at<double>(2));
     }
@@ -260,13 +276,15 @@ private:
 
     void changeLidarCoordinates(const pcl::PointCloud<pcl::PointXYZ>::Ptr &plane_cloud_cLC)
     {
-        // 1. LiDAR 평면 점들을 체커보드 좌표계로 변환
-        std::vector<cv::Point3f> lidar2cb_points =
-            transformLidarPointsToChessboard(plane_cloud_cLC, cb2cam_rvec_, cb2cam_tvec_);
+        std::vector<cv::Point3f> lidar_points;
+        for (const auto &p : plane_cloud_cLC->points)
+        {
+            lidar_points.emplace_back(
+                p.x,
+                p.y,
+                p.z);
+        }
 
-        point3fVectorToPointCloud2(lidar2cb_points, cloud_msg_, "map", this->now());
-
-        // 2. 체커보드 기준 object points 생성
         std::vector<cv::Point3f> cb_object_points;
         for (int i = 0; i < board_size_.height; i++)
         {
@@ -275,9 +293,20 @@ private:
                 cb_object_points.emplace_back(j * square_size_, i * square_size_, 0.0);
             }
         }
+        
+        cv::Mat R;
+        cv::Rodrigues(cb2cam_rvec_, R);
+        std::vector<cv::Point3f> cb2cam_points;
+        cv::transform(cb_object_points, cb2cam_points, R);
+        for (auto &pt : cb2cam_points)
+        {
+            pt.x += cb2cam_tvec_.at<double>(0);
+            pt.y += cb2cam_tvec_.at<double>(1);
+            pt.z += cb2cam_tvec_.at<double>(2);
+        }
 
         // 3. 점 개수 맞춤
-        size_t n_points = std::min(lidar2cb_points.size(), cb_object_points.size());
+        size_t n_points = std::min(lidar_points.size(), cb2cam_points.size());
         if (n_points < 3)
         {
             RCLCPP_INFO(this->get_logger(), "Not enough points for SVD computation!");
@@ -285,82 +314,46 @@ private:
         }
         // evenly sample points
         std::vector<cv::Point3f> sampled;
-        size_t step = lidar2cb_points.size() / n_points;
+        size_t step = lidar_points.size() / n_points;
         if (step == 0)
             step = 1;
 
-        for (size_t i = 0; i < lidar2cb_points.size() && sampled.size() < n_points; i += step)
+        for (size_t i = 0; i < lidar_points.size() && sampled.size() < n_points; i += step)
         {
-            sampled.push_back(lidar2cb_points[i]);
+            sampled.push_back(lidar_points[i]);
         }
-        lidar2cb_points = sampled;
+        lidar_points = sampled;
 
-        point3fVectorToPointCloud2(lidar2cb_points, checker_msg_, "map", this->now());
+        point3fVectorToPointCloud2(lidar_points, checker_msg_, "map", this->now());
 
         // 4. SVD 기반 R, t 계산
-        computeRigidTransformSVD(lidar2cb_points, cb_object_points, lidar2cb_R_, lidar2cb_t_);
-
-        std::cout << "[Lidar → Checkerboard] R:\n"
-                  << lidar2cb_R_ << std::endl;
-        std::cout << "[Lidar → Checkerboard] t:\n"
-                  << lidar2cb_t_ << std::endl;
-
-        // 저장
-        saveMultipleToFile("txt", "lidar2cb_extrinsic",
-                           "[Lidar → Checkerboard] R:\n", lidar2cb_R_,
-                           "[Lidar → Checkerboard] t:\n", lidar2cb_t_);
-
-        point3fVectorToPointCloud2(cb_object_points, cb_points_msg_, "map", this->now());
-
-        cv::Mat R_cb2cam;
-        cv::Rodrigues(cb2cam_rvec_, R_cb2cam);
-        cv::Mat R_lidar2cam = R_cb2cam * lidar2cb_R_;
-        cv::Mat t_lidar2cam = R_cb2cam * lidar2cb_t_ + cb2cam_tvec_;
+        computeRigidTransformSVD(lidar_points, cb2cam_points, lidar2cam_R_, lidar2cam_t_);
 
         std::cout << "[Lidar → Camera] R:\n"
-                  << R_lidar2cam << std::endl;
+                  << lidar2cam_R_ << std::endl;
         std::cout << "[Lidar → Camera] t:\n"
-                  << t_lidar2cam << std::endl;
+                  << lidar2cam_t_ << std::endl;
 
-        std::vector<cv::Point3f> lidar2cb_points_transformed;
-        for (const auto &pt : lidar2cb_points)
+        // 저장
+        saveMultipleToFile("txt", "lidar2cam_extrinsic",
+                           "[Lidar → Camera] R:\n", lidar2cam_R_,
+                           "[Lidar → Camera] t:\n", lidar2cam_t_);
+
+        point3fVectorToPointCloud2(cb2cam_points, cb_points_msg_, "map", this->now());
+
+        std::vector<cv::Point3f> lidar2cam_points_transformed;
+        for (const auto &pt : lidar_points)
         {
             cv::Mat pt_mat = (cv::Mat_<double>(3, 1) << pt.x, pt.y, pt.z);
-            cv::Mat pt_transformed = lidar2cb_R_ * pt_mat + lidar2cb_t_;
+            cv::Mat pt_transformed = lidar2cam_R_ * pt_mat + lidar2cam_t_;
 
-            lidar2cb_points_transformed.emplace_back(
+            lidar2cam_points_transformed.emplace_back(
                 pt_transformed.at<double>(0),
                 pt_transformed.at<double>(1),
                 pt_transformed.at<double>(2));
         }
 
-        point3fVectorToPointCloud2(lidar2cb_points_transformed, tf_points_msg_, "map", this->now());
-    }
-
-    std::vector<cv::Point3f> transformLidarPointsToChessboard(
-        const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud_cam,
-        const cv::Mat &cb2cam_rvec,
-        const cv::Mat &cb2cam_tvec)
-    {
-        cv::Mat R_cb2cam;
-        cv::Rodrigues(cb2cam_rvec, R_cb2cam);
-
-        cv::Mat R_cam2cb = R_cb2cam.t();
-        cv::Mat t_cam2cb = -R_cam2cb * cb2cam_tvec;
-
-        std::vector<cv::Point3f> points_cb;
-        for (const auto &p : cloud_cam->points)
-        {
-            cv::Mat pt_cam = (cv::Mat_<double>(3, 1) << p.x, p.y, p.z);
-            cv::Mat pt_cb = R_cam2cb * pt_cam + t_cam2cb;
-
-            points_cb.emplace_back(
-                static_cast<float>(pt_cb.at<double>(0)),
-                static_cast<float>(pt_cb.at<double>(1)),
-                static_cast<float>(pt_cb.at<double>(2)));
-        }
-
-        return points_cb;
+        point3fVectorToPointCloud2(lidar2cam_points_transformed, lidar2cam_points_, "map", this->now());
     }
 
     void computeRigidTransformSVD(
@@ -409,46 +402,6 @@ private:
                                   dst_center.z);
 
         t = dst_center_mat - R * src_center_mat;
-    }
-
-    void checkCenter(const pcl::PointCloud<pcl::PointXYZ>::Ptr &plane_cloud_cC)
-    {
-        // 체커보드 중심 계산
-        double cb_center_x = (board_size_.width - 1) * 0.5 * square_size_;
-        double cb_center_y = (board_size_.height - 1) * 0.5 * square_size_;
-        double cb_center_z = 0.0;
-
-        cv::Mat cb_center_xyz = (cv::Mat_<double>(3, 1) << cb_center_x, cb_center_y, cb_center_z);
-
-        // rvec → R
-        cv::Mat cb2cam_R;
-        cv::Rodrigues(cb2cam_rvec_, cb2cam_R);
-
-        // 체커보드 중심을 camera 좌표계로
-        cv::Mat cb2cam_center = cb2cam_R * cb_center_xyz + cb2cam_tvec_;
-
-        // LiDAR 평면 중심
-        Eigen::Vector3d lidar_center_vec(0, 0, 0);
-        for (const auto &p : plane_cloud_cC->points)
-        {
-            lidar_center_vec += Eigen::Vector3d(p.x, p.y, p.z);
-        }
-        lidar_center_vec /= plane_cloud_cC->size();
-
-        cv::Mat lidar_center_xyz = (cv::Mat_<double>(3, 1) << lidar_center_vec(0),
-                                    lidar_center_vec(1),
-                                    lidar_center_vec(2));
-
-        cv::Mat lidar2cb_center = lidar2cb_R_ * lidar_center_xyz + lidar2cb_t_;
-        cv::Mat lidar2cam_center = cb2cam_R * lidar2cb_center + cb2cam_tvec_;
-
-        cv::Mat lidar2cam_tvec = lidar2cam_center - cb2cam_R * cb_center_xyz;
-        cv::Mat lidar2cam_R = cb2cam_R * lidar2cb_R_;
-
-        std::cout << "[Lidar → Camera] R:\n"
-                  << lidar2cam_R << std::endl;
-        std::cout << "[Lidar → Camera] t:\n"
-                  << lidar2cam_tvec << std::endl;
     }
 
     void point3fVectorToPointCloud2(
