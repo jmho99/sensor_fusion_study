cf35ff79e6c53a02944f151442032e71f3b11e1d
jmho99 <bwve6g@gmail.com>
2025-07-28 14:02:22 +0900

    25/07/28

--- calib_ros2/src/multi_lidar_calib.cpp
+++ calib_ros2/src/multi_lidar_calib.cpp
@@ -18,38 +18,20 @@ public:
     MultiLidarCalibNode()
         : Node("multi_lidar_calib")
     {
-        RCLCPP_INFO(this->get_logger(), "----------------------------");
-        RCLCPP_INFO(this->get_logger(), "Start multi lidar calib node");
-        RCLCPP_INFO(this->get_logger(), "----------------------------");
+        RCLCPP_INFO(this->get_logger(), "-----------\nStart multi lidar calib node\n----------");
         filePath();
-        left_lidar_subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("/left/points", rclcpp::SensorDataQoS(),
-                                                                                            std::bind(&MultiLidarCalibNode::leftPcdCallback,
-                                                                                                      this, std::placeholders::_1));
-
-        right_lidar_subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("/right/points", rclcpp::SensorDataQoS(),
-                                                                                             std::bind(&MultiLidarCalibNode::rightPcdCallback,
-                                                                                                       this, std::placeholders::_1));
         keyboard_timer_ = this->create_wall_timer(
             std::chrono::milliseconds(100), std::bind(&MultiLidarCalibNode::keyboardCallback, this));
     }
 
 private:
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr left_lidar_subscription_;
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr right_lidar_subscription_;
+    std::vector<rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr> subscription_;
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> clouds_;
     rclcpp::TimerBase::SharedPtr keyboard_timer_;
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr left_cloud_{new pcl::PointCloud<pcl::PointXYZRGB>};
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr right_cloud_{new pcl::PointCloud<pcl::PointXYZRGB>};
-    std::string raw_left_path_, raw_right_path_;
-
-    void leftPcdCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
-    {
-        pcl::fromROSMsg(*msg, *left_cloud_);
-    }
-
-    void rightPcdCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
-    {
-        pcl::fromROSMsg(*msg, *right_cloud_);
-    }
+    
+    int number_lidars_ = 0;
+    int frame_counter_ = 0;
+    std::string origin_path_;
 
     void keyboardCallback()
     {
@@ -58,9 +40,26 @@ private:
             std::string input;
             std::getline(std::cin, input);
 
-            if (input == "s")
+            if (!input.empty() && std::all_of(input.begin(), input.end(), ::isdigit))
             {
-                savePointCloud();
+                number_lidars_ = std::stoi(input);
+                clouds_.resize(number_lidars_);
+                RCLCPP_INFO(this->get_logger(), "Start subscribe %d lidars", number_lidars_);
+
+                for (int i = 0; i < number_lidars_; i++)
+                {
+                    std::string topic_name = "/lidar" + std::to_string(i) + "/points";
+                    clouds_[i] = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+                    auto sub = this->create_subscription<sensor_msgs::msg::PointCloud2>(topic_name, rclcpp::SensorDataQoS(),
+                                                                                        [this, i, topic_name](const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+                                                                                        { pcdCallback(msg, i); });
+                    subscription_.push_back(sub);
+                }
+            }
+            else if (input == "s")
+            {
+                savePointCloud(number_lidars_, frame_counter_);
+                frame_counter_++;
             }
         }
     }
@@ -77,18 +76,29 @@ private:
     void filePath()
     {
         std::string home_dir = std::getenv("HOME");
-        RCLCPP_INFO(this->get_logger(), "Home directory : %s", home_dir);
-        std::string data_dir = home_dir + "/sensor_fusion_study_ws/src/sensor_fusion_study/calib_data";
-
-        raw_left_path_ = data_dir + "/raw_left_pcd/";
-        raw_right_path_ = data_dir + "/raw_right_pcd/";
-
-        fs::create_directories(raw_left_path_);
-        fs::create_directories(raw_right_path_);
+        RCLCPP_INFO(this->get_logger(), "Home directory : %s", home_dir.c_str());
+        std::string data_dir = home_dir + "/sensor_fusion_study_ws/src/sensor_fusion_study/calib_data/multi_lidar_calib";
+        origin_path_ = data_dir + "/origin_pointclouds/";
+        fs::create_directories(origin_path_);
     }
 
-    void savePointCloud()
+    void pcdCallback(const sensor_msgs::msg::PointCloud2::SharedPtr message, int index)
     {
+        pcl::fromROSMsg(*message, *clouds_[index]);
+    }
+
+    void savePointCloud(int num, int frame_counter)
+    {
+        RCLCPP_INFO(this->get_logger(), "Save current Point Cloud");
+        if (clouds_.empty())
+        {
+            RCLCPP_WARN(rclcpp::get_logger("savePointCloud"), "Lidar is Not Working!!!");
+        }
+        for (int i = 0; i < num; i++)
+        {
+            std::string filename = origin_path_ + "lidar" + std::to_string(i) + "_" + std::to_string(frame_counter) + ".pcd";
+            pcl::io::savePCDFile(filename, *clouds_[i]);
+        }
     }
 };
 int main(int argc, char **argv)
